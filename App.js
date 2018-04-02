e56f1f554c7cd86812d5af944c5149f2a3b1c397
saad <saad@lizard-apps.com>
2018-04-03 00:18:39 +0800

    customizing individual cards

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Card, Button } from 'react-native-elements';
 import Deck from './src/Deck';
 
 const DATA = [
@@ -16,7 +17,20 @@ const DATA = [
 export default class App extends React.Component {
   renderCard = (item) => {
     return (
-      <Text>{item.text}</Text>
+      <Card
+        key={item.id}
+        title={item.text}
+        image={{ uri: item.uri }}
+      >
+        <Text style={{ marginBottom: 10 }}>
+          I Can Customize the Card Further
+        </Text>
+        <Button
+          icon={{ name: 'code' }}
+          backgroundColor="#03A9F4"
+          title="View Now"
+        />
+      </Card>
     );
   }
 
