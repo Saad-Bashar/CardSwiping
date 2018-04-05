1183fd6cc48bc29156704acf7595f94eda1a4ec3
saad <saad@lizard-apps.com>
2018-04-05 14:38:36 +0800

    the gesture object

--- src/Deck.js
+++ src/Deck.js
@@ -10,7 +10,11 @@ class Deck extends Component {
     super(props);
 
     const panResponder = PanResponder.create({
-
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        console.log(gesture);
+      },
+      onPanResponderRelease: () => {}
     });
 
     this.state = { panResponder };
@@ -24,7 +28,7 @@ class Deck extends Component {
 
   render() {
     return (
-      <View>
+      <View {...this.state.panResponder.panHandlers }>
         {this.renderCards()}
       </View>
     )
