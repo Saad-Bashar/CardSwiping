d933c37fe34b2f8b3284db5c16955712e9af98f9
saad <saad@lizard-apps.com>
2018-04-29 01:26:05 +0800

    flash image bug fix

--- src/Deck.js
+++ src/Deck.js
@@ -92,15 +92,19 @@ class Deck extends Component {
         return (
           <Animated.View
             key={item.id}
-            style={this.getCardStyle()}
+            style={[this.getCardStyle(), styles.cardStyle]}
             {...this.state.panResponder.panHandlers }
           >
             {this.props.renderCard(item)}
           </Animated.View>
         );
       }
-      return this.props.renderCard(item);
-    });
+      return (
+        <Animated.View key={item.id} style={styles.cardStyle}>
+          {this.props.renderCard(item)}
+        </Animated.View>
+      )
+    }).reverse();
   }
 
   render() {
@@ -109,6 +113,13 @@ class Deck extends Component {
         {this.renderCards()}
       </View>
     )
+  }
+}
+
+const styles = {
+  cardStyle: {
+    position: 'absolute',
+    width: SCREEN_WIDTH
   }
 }
 
